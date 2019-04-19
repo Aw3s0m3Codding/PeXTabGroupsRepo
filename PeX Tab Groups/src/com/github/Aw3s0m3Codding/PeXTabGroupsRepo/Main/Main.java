346043b8a2a016f9eda70ab077f97fd180e551f4
Aw3s0m3 <a.stankiewicz8991@hotmail.com>
2019-04-19 17:09:51 +0200

    The first package and class of project.

---
+++ PeX Tab Groups/src/com/github/Aw3s0m3Codding/PeXTabGroupsRepo/Main/Main.java
@@ -0,0 +1,17 @@
+package com.github.Aw3s0m3Codding.PeXTabGroupsRepo.Main;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin 
+{
+
+	@Override
+	public void onEnable() {
+		
+	}
+	
+	@Override
+	public void onDisable() {
+		
+	}
+}
